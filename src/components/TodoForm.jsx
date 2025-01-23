1563269054b003658848e17bbe63a5c5bcd6b7dc
Mayankyadav3980 <ymayank501@gmail.com>
2025-01-23 17:58:10 +0530

    addtodo feature done

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useDispatch } from 'react-redux'
+import { addTodo } from '../redux/todoReducer'
 
 const TodoForm = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addTodo({ title: title, completed: false }));
+    setTitle('');
+  };
+
   return (
-    <div className='form-contianer'>
-        <input type="text" />
-        <button>Add</button>
-    </div>
-  )
+    
+      <form className="form-contianer" action="" onSubmit={handleSubmit}>
+        <input type="text" 
+        onChange={e=>setTitle(e.target.value)}
+        value={title}
+        required />
+        <button type="submit">Add</button>
+      </form>
+    
+  );
 }
 
 export default TodoForm
--- src/redux/todoReducer.js
+++ src/redux/todoReducer.js
@@ -8,13 +8,21 @@ export const getTodos = createAsyncThunk('todo/getTodos', async (args, thunkAPI)
     return data.slice(0,14);
 })
 
+export const addTodo = createAsyncThunk('todo/addTodo', async (args)=>{
+    
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos",{
+        method:'POST',
+        body:JSON.stringify({...args}),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    });
+    return res.json();
+})
 const todoSlice = createSlice({
     name:'todo',
     initialState,
     reducers:{
-        add: (state, action)=>{
-
-        },
         edit: (state, action)=>{
 
         },
@@ -25,6 +33,11 @@ const todoSlice = createSlice({
     extraReducers: (builder)=>{
         builder.addCase(getTodos.fulfilled, (state, action)=>{
             state.todos = action.payload;
+        });
+        builder.addCase(addTodo.fulfilled, (state, action)=>{
+            let len = state.todos.length;            
+            state.todos.unshift({ ...action.payload, id: len + 1 });
+            
         })
     }
 })
